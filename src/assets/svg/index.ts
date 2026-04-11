5cca0f37097b348e34a191ff4e8467c5a612b468
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-04-12 00:02:01 +0900

    🔥 chore: svgr 동작 확인용 테스트 데모 파일 제거

--- src/assets/svg/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as MedalFirst } from "./MedalFirst"
-export { default as MedalSecond } from "./MedalSecond"
-export { default as MedalThird } from "./MedalThird"
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router"
 
-import { MedalFirst, MedalSecond, MedalThird } from "@/assets/svg"
-
 export const Route = createFileRoute("/")({
   component: HomePage,
 })
 
 function HomePage() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center gap-4">
-      <MedalFirst className="h-20 w-20" />
-      <MedalSecond className="h-20 w-20" />
-      <MedalThird className="h-20 w-20" />
-    </div>
-  )
+  return <div>UMC Product</div>
 }
