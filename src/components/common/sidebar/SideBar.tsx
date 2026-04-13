7c2111c3ba3f4cb1465a003ad8bbf0715a2f48fd
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-04-13 14:45:05 +0900

    ✨ feat: SideBar에 className prop 추가

--- src/components/common/sidebar/SideBar.tsx
+++ src/components/common/sidebar/SideBar.tsx
@@ -1,19 +1,30 @@
 import { useState } from "react"
 
+import { cn } from "@/lib/utils"
+
 import { SideBarDropDown } from "./dropdown/SideBarDropDown"
 import { SideBarItem } from "./menu/SideBarItem"
 import { SideBarMenuItem } from "./menu/SideBarMenuItem"
 import { SIDEBAR_ITEMS } from "./sidebar.config"
 
+interface SideBarProps {
+  className?: string
+}
+
 const DEMO_DAY_EDITION = 10
 
-export default function SideBar() {
+export default function SideBar(className: SideBarProps) {
   const [openTitle, setOpenTitle] = useState<string>(
     SIDEBAR_ITEMS[0]?.title ?? "",
   )
 
   return (
-    <aside className="border-teal-gray-200 flex h-fit w-55 flex-col items-center justify-start border-r py-4">
+    <aside
+      className={cn(
+        "border-teal-gray-200 flex h-fit w-55 shrink-0 flex-col items-center justify-start border-r pt-4",
+        { ...className },
+      )}
+    >
       <SideBarDropDown />
       <section className="flex flex-col py-4">
         <span className="text-body-3-regular text-teal-gray-400 mb-2 pl-0.5">
