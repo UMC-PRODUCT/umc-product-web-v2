f2e3368781210878a525a521eb34e51f2eb34d35
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-04-13 19:10:31 +0900

    ✨ feat: 공용 Toast 컴포넌트 구현
    - cva 기반 color(primary/red) x variant(deep/weak) 조합
    - type:default(닫기 버튼) / type:time(카운트다운) 지원
    - zustand store로 전역 토스트 관리
    - FIFO 순서로 오래된 토스트부터 fade-out
    - 최신 토스트가 front, 최대 3개 카드 스택
    - 진입 시 shake 애니메이션, 퇴장 시 fade-out

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,7 @@
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
 
+import { ToastProvider } from "@/components/common/toast/ToastProvider"
+
 import type { QueryClient } from "@tanstack/react-query"
 
 interface RouterContext {
@@ -14,6 +16,7 @@ function RootComponent() {
   return (
     <div className="bg-teal-gray-50 h-full min-h-screen max-w-full min-w-fit pb-12">
       <Outlet />
+      <ToastProvider />
     </div>
   )
 }
