94568ddcf7ccd3a386fa72acb02f0d19142a4587
OCJune <cjbormk11@gmail.com>
2026-04-22 02:39:58 +0900

    ✨ feat: 체크박스 컴포넌트 추가

--- src/features/notice/index.ts
+++ src/features/notice/index.ts
@@ -1,5 +1,6 @@
 export { Border } from "./ui/Border"
 export { type Chapter, CHAPTERS, ChapterSelector } from "./ui/ChapterSelector"
+export { CheckBox } from "./ui/CheckBox"
 export { NoticeCard } from "./ui/NoticeCard"
 export { NoticeCardList, type NoticeItem } from "./ui/NoticeCardList"
 export { NoticeSubmitButton } from "./ui/NoticeSubmitButton"
