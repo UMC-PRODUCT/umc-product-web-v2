be67902ac4ab22408b46ae40a8ca9a713344168e
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-04-22 11:30:53 +0900

    🎨 style: caption-3-medium 타이포그래피 토큰 추가 및 CheckIcon 스트로크 상속 개선

--- src/shared/assets/icon/check/CheckIcon.tsx
+++ src/shared/assets/icon/check/CheckIcon.tsx
@@ -4,13 +4,13 @@ const CheckIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 16 16"
+    strokeWidth={1.5}
     {...props}
   >
     <path
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={1.5}
       d="M4 7.77 6.8 11 12 5"
     />
   </svg>
