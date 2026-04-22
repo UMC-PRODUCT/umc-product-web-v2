c9e8823066e9dc71fd52363e411c2b7dad014ba6
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-04-22 11:31:09 +0900

    🍱 chore: toggle 아이콘 4종 추가

---
+++ src/shared/assets/icon/toggle/ToggleTextIcon.tsx
@@ -0,0 +1,15 @@
+import type { SVGProps } from "react"
+const ToggleTextIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 30 30"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M20.5 19a1.5 1.5 0 0 1 0 3h-17a1.5 1.5 0 0 1 0-3zm6-10a1.5 1.5 0 0 1 0 3h-23a1.5 1.5 0 0 1 0-3z"
+    />
+  </svg>
+)
+export default ToggleTextIcon
