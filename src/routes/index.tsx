578cbd95e1f14f41989747a22d7f6662b81133c5
OCJune <cjbormk11@gmail.com>
2026-04-24 20:18:27 +0900

    ✨ feat: 홈페이지 리다이렉트에 기본 검색 파라미터 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,9 @@ import { createFileRoute, redirect } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
   beforeLoad: () => {
-    throw redirect({ to: "/matching" })
+    throw redirect({
+      to: "/matching",
+      search: { chapter: "Chromium", page: 1 }, // TODO: 사용자 지부로 리다이렉트
+    })
   },
 })
