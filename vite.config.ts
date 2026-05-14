5600864a74861193afef161ee0f2cd818cc76b82
hyewonimdang <hyehyekuk202@gmail.com>
2026-05-15 00:28:47 +0900

    🐛 fix: CORS 해결을 위해 proxy 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,36 @@ import tailwindcss from "@tailwindcss/vite"
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import path from "node:path"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import svgr from "vite-plugin-svgr"
 
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({
-      routesDirectory: "./src/routes",
-      generatedRouteTree: "./src/routeTree.gen.ts",
-      autoCodeSplitting: true,
-    }),
-    react(),
-    svgr(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  const apiOrigin = new URL(env.VITE_API_BASE_URL).origin
+
+  return {
+    plugins: [
+      TanStackRouterVite({
+        routesDirectory: "./src/routes",
+        generatedRouteTree: "./src/routeTree.gen.ts",
+        autoCodeSplitting: true,
+      }),
+      react(),
+      svgr(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiOrigin,
+          changeOrigin: true,
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  }
 })
