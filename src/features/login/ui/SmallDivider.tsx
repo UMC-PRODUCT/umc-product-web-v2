db082e72d51e1c0c531459701d77d67ab3911335
OCJune <cjbormk11@gmail.com>
2026-05-15 20:42:33 +0900

    ♻️ refactor: 필요 없는 클래스 삭제

--- src/features/login/ui/SmallDivider.tsx
+++ src/features/login/ui/SmallDivider.tsx
@@ -1,3 +1,3 @@
 export function SmallDivider() {
-  return <div className="bg-teal-gray-300 flex h-3 w-0.5 rounded-[6px]"></div>
+  return <div className="bg-teal-gray-300 h-3 w-0.5 rounded-[6px]"></div>
 }
