315759c8f48c47aec88d2a71cd66ade8613751e7
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-06-19 03:30:52 +0900

    🎨 style: 프로젝트 중단 메뉴 라벨 조정

--- src/features/project/management/ui/ProjectManagementMoreMenu.tsx
+++ src/features/project/management/ui/ProjectManagementMoreMenu.tsx
@@ -412,7 +412,7 @@ export function ProjectManagementMoreMenu({
               {status === "IN_PROGRESS" &&
                 (isPermissionLoading || canPublishProject) && (
                   <DropdownItem
-                    label="매칭 중단 (복구 불가)"
+                    label="중단 (복구 불가)"
                     disabled={isPermissionLoading}
                     onClick={handleAbortClick}
                     className="text-error-500"
