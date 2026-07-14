47d7f8db80171e6e4a6ee1aaeff42139e8e03b82
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-07-15 01:37:41 +0900

    💄 style: TextButton hover 밑줄 간격 확대

--- src/shared/ui/button/TextButton.test.tsx
+++ src/shared/ui/button/TextButton.test.tsx
@@ -15,7 +15,7 @@ describe("TextButton", () => {
       "leading-6",
       "text-teal-gray-500",
       "hover:underline",
-      "hover:underline-offset-2",
+      "hover:underline-offset-4",
     )
   })
 
