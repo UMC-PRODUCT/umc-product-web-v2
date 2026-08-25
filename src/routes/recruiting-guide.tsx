577c6a4c96b8f8d1a04f0dbd57dcd343b0e1cb0c
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-08-25 21:08:32 +0900

    🐛 fix: 정적 랜딩 헤더 인증 의존성 제거

--- src/routes/recruiting-guide.tsx
+++ src/routes/recruiting-guide.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router"
 
 import { RecruitingGuidePage } from "@/features/about/ui/RecruitingGuidePage"
 import { createMeta, SITE_URL } from "@/shared/seo"
-import RecruitingHeader from "@/widgets/navigation/header/RecruitingHeader"
+import PublicLandingHeader from "@/widgets/navigation/header/PublicLandingHeader"
 
 export const Route = createFileRoute("/recruiting-guide")({
   head: () =>
@@ -18,7 +18,7 @@ function RecruitingGuideRoute() {
   return (
     <div className="relative">
       <div className="sticky top-0 z-50 -mb-20">
-        <RecruitingHeader tone="glass" />
+        <PublicLandingHeader />
       </div>
       <RecruitingGuidePage />
     </div>
